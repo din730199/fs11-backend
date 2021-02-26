0a63faf9eb6172a7ddccffb5a8e7ab0d74fa65fa
nhan <dinhnhan3007@gmail.com>
2021-02-26 12:45:59 +0700

    update trip controller

--- controllers/trip.js
+++ controllers/trip.js
@@ -12,20 +12,27 @@ const postBookTrip = async (req, res) => {
     const foundedTrip = await trip.findById(tripID).session(session);
     if (!foundedTrip)
       return res.status(400).send({msg: 'Invalid trip, Id is not exist'});
+    //check id seat
+    let foundedSeat = [];
+    for (const e of seatID) {
+      check = foundedTrip.seat.findIndex(
+        (item) => item._id.toString() === e && item.status === 'avaiable'
+      );
+      foundedSeat.push(check);
+    }
 
-    const foundedIndex = foundedTrip.seat.findIndex(
-      (item) => item._id.toString() === seatID && item.status === 'avaiable'
-    );
-    if (foundedIndex === -1)
-      return res.status(400).send({msg: 'Invalid seat, Id is not exist'});
+    const checkSeat = foundedSeat.some((item) => item === -1);
+    if (checkSeat === true) return res.status(400).send({msg: 'Invalid seat'});
 
     //update trạng thái ghế
-    foundedTrip.seat[foundedIndex].userId = req.user._id;
-    foundedTrip.seat[foundedIndex].status = 'booked';
+    let arraySeat = [];
+    foundedSeat.forEach((e) => {
+      foundedTrip.seat[e].userId = req.user._id;
+      foundedTrip.seat[e].status = 'booked';
+      arraySeat.push(foundedTrip.seat[e]);
+    });
 
     await foundedTrip.save();
-    console.log(req.user._id);
-    console.log('save seat');
 
     //tạo ticket
     await ticketModel.create(
@@ -33,13 +40,11 @@ const postBookTrip = async (req, res) => {
         {
           user: req.user._id,
           trip: tripID,
-          seat: [foundedTrip.seat[foundedIndex]],
+          seat: arraySeat,
         },
       ],
       {session: session}
     );
-
-    console.log('save ticket');
 
     await session.commitTransaction();
     session.endSession();
@@ -51,5 +56,55 @@ const postBookTrip = async (req, res) => {
     res.status(500).send({err: 'Server error'});
   }
 };
+
+// const postBookTrip = async (req, res) => {
+//   const {tripID, seatID} = req.body;
+
+//   const session = await mongoose.startSession();
+//   await session.startTransaction();
+
+//   try {
+//     const foundedTrip = await trip.findById(tripID).session(session);
+//     if (!foundedTrip)
+//       return res.status(400).send({msg: 'Invalid trip, Id is not exist'});
+
+//     const foundedIndex = foundedTrip.seat.findIndex(
+//       (item) => item._id.toString() === seatID && item.status === 'avaiable'
+//     );
+//     if (foundedIndex === -1)
+//       return res.status(400).send({msg: 'Invalid seat, Id is not exist'});
+
+//     //update trạng thái ghế
+//     foundedTrip.seat[foundedIndex].userId = req.user._id;
+//     foundedTrip.seat[foundedIndex].status = 'booked';
+
+//     await foundedTrip.save();
+//     console.log(req.user._id);
+//     console.log('save seat');
+
+//     //tạo ticket
+//     await ticketModel.create(
+//       [
+//         {
+//           user: req.user._id,
+//           trip: tripID,
+//           seat: [foundedTrip.seat[foundedIndex]],
+//         },
+//       ],
+//       {session: session}
+//     );
+
+//     console.log('save ticket');
+
+//     await session.commitTransaction();
+//     session.endSession();
+
+//     res.send({msg: 'success'});
+//   } catch (error) {
+//     await session.abortTransaction();
+//     session.endSession();
+//     res.status(500).send({err: 'Server error'});
+//   }
+// };
 
 module.exports = {postBookTrip};
